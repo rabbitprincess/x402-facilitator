ed91cfd8a6dcf2b787b39e7307e7abd35ed6af02
iwanhae <iwanhae@gmail.com>
2025-05-19 14:50:51 +0900

    chore: remove unused error type definition
    - Deleted the Error struct from the types/error.go file as it is no longer needed in the codebase.

--- types/error.go
+++
@@ -1,9 +0,0 @@
-package types
-
-import "encoding/json"
-
-type Error struct {
-	Code    string                     `json:"code"`
-	Message string                     `json:"message"`
-	Extra   map[string]json.RawMessage `json:"extra,omitempty"`
-}
